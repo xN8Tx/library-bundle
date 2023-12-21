f1e1d5664002a671b016eb3fb4a3d57296186ff2
Evgeniy Shteyn <theeldern8t@ya.ru>
2023-12-22 06:37:37 +0700

    feat: move all to template folder

--- bin/cli.js
+++ bin/cli.js
@@ -1,34 +1,59 @@
 #! /usr/bin/env node
 
-import { execSync } from 'child_process';
+import { execSync } from "child_process";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import fse from "fs-extra";
+import path from "path";
 
 const useCommand = async (command) => {
   try {
-    execSync(command, { stdio: 'inherit' });
+    execSync(command, { stdio: "inherit" });
     return true;
   } catch (error) {
-    console.log(`⚠️Failed to run ${command}. Error:\n`, error);
+    console.log(`⚠️ Failed to run ${command}. Error:\n`, error);
     return false;
   }
 };
 
-const PROJECT_NAME = process.argv[2];
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const PROJECT_NAME = process.argv[2] === "." ? "" : process.argv[2];
+
+const LIBRARY_PATH = path.dirname(__dirname);
+const LIBRARY_TEMPLATE_PATH = path.join(LIBRARY_PATH, "template");
+const PROJECT_PATH = path.join(process.cwd(), PROJECT_NAME);
 
 // Commands
-const cloneRepository = `git clone https://github.com/xN8Tx/ts-rlb ${PROJECT_NAME}`;
-const installDepends = `cd ${PROJECT_NAME} && npm install`;
+const installDepends = `cd ${PROJECT_PATH} && npm install`;
+const moveFiles = async () => {
+  try {
+    await fse.ensureDir(PROJECT_PATH); // check if we have dir if not create
+    await fse.copy(LIBRARY_TEMPLATE_PATH, PROJECT_PATH);
+    return true;
+  } catch (err) {
+    console.log("⚠️ Error copying files. Error:", err);
+    return false;
+  }
+};
 
 // Run
-console.log('Start to tune the project✨');
+console.log("Start configure the project✨");
+console.log("==================================");
 
-console.log('Move boilerplate to your project✨');
-const cloneRepositoryRun = useCommand(cloneRepository);
-if (!cloneRepositoryRun) process.exit(-1);
+console.log("Move boilerplate to your project✨");
+const movingFile = await moveFiles();
+if (!movingFile) process.exit(-1);
+console.log("Successfully move boilerplate to your project🥳");
 
-console.log('Install all dependencies✨');
+console.log("Install all dependencies✨");
 const installDependsRun = useCommand(installDepends);
 if (!installDependsRun) process.exit(-1);
+console.log("Successfully install all dependencies✨");
 
-console.log('Congratulation! Successfully tune the project🥳');
-console.log(`Next step is run:`);
+console.log("==================================");
+console.log("Congratulation! Successfully configure the project🥳");
+console.log("");
+console.log(`To start development run: `);
 console.log(`cd ${PROJECT_NAME} && npm run storybook`);
